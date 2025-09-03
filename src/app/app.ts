90203a5984b2b3bbe6abb8dd454416f5d95bb8d9
joaopaulofserafim <jpfserafim72@gmail.com>
2025-09-02 22:17:12 -0300

    Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angularcrud2');
+  protected readonly title = ('Ola, Mundo!');
+  protected readonly paragrf = ('Este é o meu primeiro projeto em Angular!');
 }
+
+
